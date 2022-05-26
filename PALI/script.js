abc1e113d258f5033881dced8df8dc33c5fa666a
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-26 22:34:12 +0200

    mdofiche

--- PALI/script.js
+++ PALI/script.js
@@ -13,13 +13,50 @@ Creare una funzione per capire se la parola inserita è palindroma
 */
 
 let parola = prompt("inserisci una parola");
+const parolaInserita="osso";
+const parolaScomposta = ["o","s","s","o"];
+console.log ("osso".split(""));
 
-console.log(parola);
 
-if(parola="forzaroma"){
-    console.log("le parole sono uguali")
+console.log("parolaInserita")
+
+// if(parola=="osso"){
+//     console.log("la parola è palindroma")
+// }else
+// {   console.log("le parole non sono uguali")
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*dichiaro funzione
+function stampaMessaggioBenvenuto(){
+    console.log("benvenuto utente");
+}
+//invoco la funzione
+stampaMessaggioBenvenuto();
+
+//Funzione con argomenti
+
+// const num1=59;
+// const num2=12;
+
+function sommaNumeri(num1, num2){
+    console.log(num1 + num2)
 }
 
+sommaNumeri(45,12)
 
 
 
