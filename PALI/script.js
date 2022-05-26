aeb102d823c531aa6e90eca4634a51194cb42c26
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-26 15:50:27 +0200

    mod

---
+++ PALI/script.js
@@ -0,0 +1,32 @@
+/*
+
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+*/
+
+
+//_____________________RAGIONAMENTO________________________
+
+/* 1) Prompt per chiedere all'utente di inserire una parola 
+    2) creare un array dove salvare le parole inserite
+    3) entrare nell'array per prendere una parola
+    4) creare funzione per capire se è palindroma
+*/
+
+
+let parolaInserita
+parolaInserita = [];
+console.log(parolaInserita);
+
+
+const listaParoleInserite =[]
+console.log(listaParoleInserite)
+
+
+/*
+function (parola,palindroma) {
+    
+}
+*/
+
