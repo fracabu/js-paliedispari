fa7f58f45fefddac7580d4ecba5042ab844fe0b4
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-26 16:01:01 +0200

    mod

--- PALI/script.js
+++ PALI/script.js
@@ -9,9 +9,7 @@ Creare una funzione per capire se la parola inserita è palindroma
 //_____________________RAGIONAMENTO________________________
 
 /* 1) Prompt per chiedere all'utente di inserire una parola 
-    2) creare un array dove salvare le parole inserite
-    3) entrare nell'array per prendere una parola
-    4) creare funzione per capire se è palindroma
+    2) creare funzione per capire se è palindroma
 */
 
 let parola = prompt("inserisci una parola");
